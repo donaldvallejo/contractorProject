fde71857d28e542d24c55636ae5e1802612d1fc2
Donald Vallejo <donny.vallejo@gmail.com>
2019-10-08 17:37:09 -0700

    improved logic for simulation should run function

--- app.py
+++ app.py
@@ -15,16 +15,16 @@ app = Flask(__name__)
 @app.route('/')
 def cars_index():
   """Show all cars"""
-  return render_template('playlists_index.html', cars=cars.find())
+  return render_template('cars_index.html', cars=cars.find())
 
 @app.route('/cars/new')
 def cars_new():
-  """Create Playlists"""
+  """Create cars"""
   return render_template('cars_new.html', car = {}, title='New Cars')
 
 @app.route('/cars', methods=['POST'])
 def cars_submit():
-  """Submit playlists to database"""
+  """Submit cars to database"""
   car = {
     'Make': request.form.get('Make'),
     'Model': request.form.get('Model'),
@@ -39,23 +39,23 @@ def cars_submit():
   print("Check to see if Data is even happening \n", car_id, car)
   return redirect(url_for('cars_show', car_id=car_id))
 
-@app.route('/playlists/<playlist_id>')
-def playlists_show(playlist_id):
-    """Show a single playlist."""
-    playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
-    playlist_comments = comments.find({'playlist_id': ObjectId(playlist_id)})
-    return render_template('playlists_show.html', playlist=playlist, comments=playlist_comments)
+@app.route('/cars/<car_id>')
+def cars_show(car_id):
+    """Show a single car."""
+    car = cars.find_one({'_id': ObjectId(car_id)})
+    car_comments = comments.find({'car_id': ObjectId(car_id)})
+    return render_template('cars_show.html', car=car, comments=car_comments)
 
-@app.route('/playlists/<playlist_id>/edit')
-def playlists_edit(playlist_id):
-    """Show the edit form for a playlist."""
-    playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
-    return render_template('playlists_edit.html', playlist=playlist, title='Edit Playlist')
+@app.route('/cars/<car_id>/edit')
+def cars_edit(car_id):
+    """Show the edit form for a car."""
+    car = cars.find_one({'_id': ObjectId(car_id)})
+    return render_template('cars_edit.html', car=car, title='Edit Car')
 
-@app.route('/playlists/<playlist_id>', methods=['POST'])
-def playlists_update(playlist_id):
-  """Submit an edited playlists"""
-  updated_playlist = {
+@app.route('/cars/<car_id>', methods=['POST'])
+def cars_update(car_id):
+  """Submit an edited cars"""
+  updated_car = {
       'Make': request.form.get('Make'),
       'Model': request.form.get('Model'),
       'Description': request.form.get('Description'),
@@ -63,34 +63,34 @@ def playlists_update(playlist_id):
       'Price': request.form.get('Price'),
       'Image': request.form.get('Image'),
   }
-  playlists.update_one(
-    {'_id': ObjectId(playlist_id)},
-    {'$set': updated_playlist})
-  return redirect(url_for('playlists_show', playlist_id=playlist_id))
+  cars.update_one(
+    {'_id': ObjectId(car_id)},
+    {'$set': updated_car})
+  return redirect(url_for('cars_show', car_id=car_id))
 
-@app.route('/playlists/<playlist_id>/delete', methods=['POST'])
-def playlists_delete(playlist_id):
-    """Delete one playlist."""
-    playlists.delete_one({'_id': ObjectId(playlist_id)})
-    return redirect(url_for('playlists_index'))
+@app.route('/cars/<car_id>/delete', methods=['POST'])
+def cars_delete(playlist_id):
+    """Delete one car."""
+    cars.delete_one({'_id': ObjectId(car_id)})
+    return redirect(url_for('cars_index'))
 
-@app.route('/playlists/comments', methods=['POST'])
+@app.route('/cars/comments', methods=['POST'])
 def comments_new():
     """Submit a new comment."""
     comment = {
         'title': request.form.get('title'),
         'content': request.form.get('content'),
-        'playlist_id': ObjectId(request.form.get('playlist_id'))
+        'car_id': ObjectId(request.form.get('car_id'))
     }
     comment_id = comments.insert_one(comment).inserted_id
-    return redirect(url_for('playlists_show', playlist_id=request.form.get('playlist_id')))
+    return redirect(url_for('cars_show', car_id=request.form.get('car_id')))
 
-@app.route('/playlists/comments/<comment_id>', methods=['POST'])
+@app.route('/cars/comments/<comment_id>', methods=['POST'])
 def comments_delete(comment_id):
     """Action to delete a comment."""
     comment = comments.find_one({'_id': ObjectId(comment_id)})
     comments.delete_one({'_id': ObjectId(comment_id)})
-    return redirect(url_for('playlists_show', playlist_id=comment.get('playlist_id')))
+    return redirect(url_for('cars_show', car_id=comment.get('car_id')))
 
 if __name__ == '__main__':
   app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
