118f9215b4e7cd732e8b68b7576f27520e3a3d6f
Donald Vallejo <donny.vallejo@gmail.com>
2019-10-10 18:54:07 -0700

    pushed all tests

--- test.py
+++ test.py
@@ -2,6 +2,8 @@ from app import app
 from unittest import TestCase, main as unittest_main
 from unittest import TestCase, main as unittest_main, mock
 from bson.objectid import ObjectId
+# import MutableMapping
+# import Mapping
 
 sample_car_id = ObjectId('5d55cffc4a3d4031f42827a3')
 sample_car = {
@@ -10,8 +12,8 @@ sample_car = {
     'Description': 'an ok car',
     'Color': 'white',
     'Price': '25000'
-
 }
+
 sample_form_data = {
     'Make': sample_car['Make'],
     'Model': sample_car['Model'],
@@ -25,17 +27,35 @@ class carsTests(TestCase):
         self.client = app.test_client()
         app.config['TESTING'] = True
 
-def test_index(self):
-        """Test the cars homepage."""
-        result = self.client.get('/')
+    def test_index(self):
+            """Test the cars homepage."""
+            result = self.client.get('/')
+            self.assertEqual(result.status, '200 OK')
+            self.assertIn(b'car', result.data)
+        
+    def test_new(self):
+            """Test the new car creation page."""
+            result = self.client.get('/cars/new')
+            self.assertEqual(result.status, '200 OK')
+            self.assertIn(b'Cars for days', result.data)
+
+    @mock.patch('pymongo.collection.Collection.find_one')
+    def test_show_car(self, mock_find):
+        """Test showing a single car."""
+        mock_find.return_value = sample_car
+
+        result = self.client.get(f'/cars/{sample_car_id}')
         self.assertEqual(result.status, '200 OK')
-        self.assertIn(b'car', result.data)
-    
-def test_new(self):
-        """Test the new car creation page."""
-        result = self.client.get('/cars/new')
+        self.assertIn(b'Cars for days', result.data)
+
+    @mock.patch('pymongo.collection.Collection.find_one')
+    def test_edit_car(self, mock_find):
+        """Test editing a single car."""
+        mock_find.return_value = sample_car
+
+        result = self.client.get(f'/cars/{sample_car_id}/edit')
         self.assertEqual(result.status, '200 OK')
-        self.assertIn(b'New car', result.data)
+        self.assertIn(b'Cars for days', result.data)
 
 if __name__ == '__main__':
     unittest_main()
